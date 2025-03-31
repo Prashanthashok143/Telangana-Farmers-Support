9eb7f20cf6c9d041ce0881077c1faacbda192048
Prashanth <Prashanthashok143@gmail.com>
2025-03-31 07:57:38 +0530

    mongodb url updated

--- server-side/config/database.js
+++ server-side/config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/telangana_farmers";
 exports.dbConnect = () => {
   mongoose
-    .connect("mongodb://localhost:27017/telangana_farmers")
+    .connect(mongoURI)
     .then(() => console.log("DB is connected"))
     .catch((err) => console.log(err));
 };

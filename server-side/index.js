da01df85bed40e7c27a239d22721afbf77e3bf69
SHANIGARAPU PRASHANTH <151112707+Prashanthashok143@users.noreply.github.com>
2025-04-01 13:39:57 +0530

    Update index.js
    cors updated

--- server-side/index.js
+++ server-side/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:true}));
 app.use('/uploads', express.static('uploads'));
 app.use("/uploadScheme", express.static("uploadScheme"));
 app.use(cors())
-app.use(cors({ origin: "https://telangana-farmers-support-qxyq3bfm7.vercel.app/", credentials: true }));
+// app.use(cors({ origin: "https://telangana-farmers-support-qxyq3bfm7.vercel.app/", credentials: true }));
 
 
 
